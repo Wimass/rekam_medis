a9ba822c5bb59b16c2d4ad20db3ca19344c1cabb
wimass <wimasmutassubkhan@gmail.com>
2023-02-03 14:09:17 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    //...
+    plugins: [require("daisyui")],
+}
+
+
+module.exports = {
+
+    plugins: [
+        require('flowbite/plugin')
+    ]
+
+}
+ 
+module.exports = {
+
+    content: [
+        "./node_modules/flowbite/**/*.js"
+    ]
+
+}
